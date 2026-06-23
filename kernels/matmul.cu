44943843ba15aad3ce8e6bbdda1ebddf50cce522
Sean Zhang <ss0486034@gmail.com>
2026-06-23 17:13:06 -0400

    second trial with cmake

--- kernels/matmul.cu
+++ kernels/matmul.cu
@@ -2,25 +2,29 @@
 __global__ void matmul_naive(const float* A, const float* B, float* C, int N){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    for(int i =0; i < N; i++){
-        C[row * N + col] += A[row * N + i] * B[i * N + col];
+    float sum = 0.0f;
+    for(int i =0; i < N && row < N && col < N; i++){
+        sum += A[row * N + i] * B[i * N + col];
     }
+    C[row * N + col] = sum;
 }
 
 __global__ void matmul_tiled(const float* A, const float* B, float* C, int N){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int tile_size = N/32;
-    for(int i = 0; i<N; i+=tile_size){
-        __shared__ float A_tile[32][32];
-        __shared__ float B_tile[32][32];
+    int tile_size = 32;
+    float sum = 0.0f;
+    __shared__ float A_tile[32][32];
+    __shared__ float B_tile[32][32];
+    for(int i = 0; i<N && row < N && col < N; i+=tile_size){
         A_tile[threadIdx.y][threadIdx.x] = A[row * N + (i + threadIdx.x)];
         B_tile[threadIdx.y][threadIdx.x] = B[(i + threadIdx.y) * N + col];
         __syncthreads();
         for(int j = 0; j<tile_size; j++){
-            C[row * N + col] += A_tile[threadIdx.y][j] * B_tile[j][threadIdx.x];
+            sum += A_tile[threadIdx.y][j] * B_tile[j][threadIdx.x];
         }
         __syncthreads();
     } 
+    C[row * N + col] = sum;
 }
 
