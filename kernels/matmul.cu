93acd7d7dc7c736ab05bb8faf3ec61390ca47031
Sean Zhang <ss0486034@gmail.com>
2026-06-24 16:56:33 -0400

    created gemm benchmark and matmul for foloat_4

--- kernels/matmul.cu
+++ kernels/matmul.cu
@@ -31,3 +31,42 @@ __global__ void matmul_tiled(const float* A, const float* B, float* C, int N){
 
 }
 
+__global__ void float4_vectorized(const float* A, const float* B, float* C, int N){
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int tile_size = 128;
+    float sum = 0.0f;
+    __shared__ float A_tile[32][128];
+    __shared__ float B_tile[32][128];
+    for(int i = 0; i < N; i += tile_size){
+        if(i+threadIdx.x * 4 < N && row < N && i+threadIdx.y < N && col*4 < N){
+            float4 a4 = reinterpret_cast<const float4*>(A)[(row * N + i + threadIdx.x * 4) / 4];
+            A_tile[threadIdx.y][threadIdx.x * 4 + 0] = a4.x;
+            A_tile[threadIdx.y][threadIdx.x * 4 + 1] = a4.y;
+            A_tile[threadIdx.y][threadIdx.x * 4 + 2] = a4.z;
+            A_tile[threadIdx.y][threadIdx.x * 4 + 3] = a4.w;
+            float4 b4 = reinterpret_cast<const float4*>(B)[((i + threadIdx.y) * N + col * 4) / 4];
+            B_tile[threadIdx.y][threadIdx.x * 4 + 0] = b4.x;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 1] = b4.y;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 2] = b4.z;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 3] = b4.w;
+        }else{
+            A_tile[threadIdx.y][threadIdx.x * 4 + 0] = 0.0f;
+            A_tile[threadIdx.y][threadIdx.x * 4 + 1] = 0.0f;
+            A_tile[threadIdx.y][threadIdx.x * 4 + 2] = 0.0f;
+            A_tile[threadIdx.y][threadIdx.x * 4 + 3] = 0.0f;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 0] = 0.0f;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 1] =  0.0f;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 2] = 0.0f;
+            B_tile[threadIdx.y][threadIdx.x * 4 + 3] = 0.0f;    
+        }
+            __syncthreads();
+            for(int j = 0; j < tile_size; j++)
+                sum += A_tile[threadIdx.y][j] * B_tile[j][threadIdx.x];
+            __syncthreads();
+        }
+    
+    if(row < N && col < N)
+        C[row * N + col] = sum;
+}
+
