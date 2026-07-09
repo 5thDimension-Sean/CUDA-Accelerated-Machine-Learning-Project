ee605aa9b3246443a35453f4b72cfbeaf48828d9
Sean Zhang <ss0486034@gmail.com>
2026-07-09 12:27:54 -0400

    changed ofrmatting for layer.cu

--- network/layer.cu
+++ network/layer.cu
@@ -3,9 +3,8 @@
 #include <stdio.h>
 #include <cmath>
 #include <iostream>
-#include "batchnorm.cu"
-#include "activations.cu"
-#include "pooling.cu"
+#include "layer.cuh"
+
 
 // TODO — Week 5: implement here
 #define CUDA_CHECK(call)                                                        \
@@ -18,26 +17,7 @@
         }                                                                       \
     } while (0)
 
-enum LayerType {
-    CONV, 
-    BATCHNORM,
-    RELU, 
-    FC, 
-};
 
-struct Layer {
-    LayerType type;
-    int in_H, in_W, in_C;      // input dimensions
-    int out_H, out_W, out_C;   // output dimensions (computed at setup)
-    float *d_output;           // this layer's output, allocated once, reused each forward
-    float *d_weights;          // CONV/FC only; null for RELU/POOL/BATCHNORM
-    float *d_bias;             // CONV/FC/BATCHNORM; null for RELU/POOL
-    float *d_input;            // cached input pointer, needed by backward
-    float *d_grad_weights;     // gradient w.r.t. weights
-    float *d_grad_bias;        // gradient w.r.t. bias
-    int P, S;                  // POOL: window size, stride
-    float epsilon;             // BATCHNORM
-};
 
 void layer_setup(Layer *layer){
 
