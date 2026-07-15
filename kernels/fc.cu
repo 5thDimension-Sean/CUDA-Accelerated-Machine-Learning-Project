f0b977a87bb29de531e52c74a5f474c583eabea4
Sean Zhang <ss0486034@gmail.com>
2026-07-15 13:24:23 -0400

    optimization of optimizer.cu and added basis of fc.cu

--- kernels/fc.cu
+++ kernels/fc.cu
@@ -23,3 +23,11 @@ __global__ void matmul_tiled(const float* A, const float* B, float* C, int N){
 
 }
 */
+
+__global__ void sgd_kernel(float *weights, const float *grad, float lr, int n){
+
+}
+
+void fc_forward(const float* X, const float *W, const float *b, float *Y, int batch, int in, int out){
+    
+}
--- network/optimizer.cu
+++ network/optimizer.cu
@@ -1,17 +1,7 @@
 #include "optimizer.cuh"
+#include "../kernels/common.cuh"
 #include <cmath>
 #include <cstdio>
-
-#define CUDA_CHECK(call)                                                        \
-    do {                                                                        \
-        cudaError_t err = (call);                                               \
-        if (err != cudaSuccess) {                                               \
-            fprintf(stderr, "CUDA error at %s:%d - %s\n",                      \
-                    __FILE__, __LINE__, cudaGetErrorString(err));               \
-            exit(EXIT_FAILURE);                                                 \
-        }                                                                       \
-    } while (0)
-
 
 //optimizer.cu
 
