86daf4e6b21607aeb0cd347517a4257ec7a075a9
Sean Zhang <ss0486034@gmail.com>
2026-07-15 12:48:18 -0400

    fix in cuh to change names

--- network/optimizer.cuh
+++ network/optimizer.cuh
@@ -1,27 +1,36 @@
-#ifndef NETWORK_OPTIMZER_CUH
-#define NETWORK_OPTIMZER_CUH
+#ifndef NETWORK_LOSS_CUH
+#define NETWORK_LOSS_CUH
 
 #include <cuda_runtime.h>
 
 enum class LossType
 {
     SGD, 
-    Momentum
+    MOMENTUM
 };
 
-__global__ void loss_forward_kernel(const float* predictions,
+__global__ void sgd_kernel(const float* predictions,
                                     const float* targets,
                                     float* loss,
                                     int numElements,
                                     LossType lossType);
 
+__global__ void momentum_kernel(const float* predictions,
+                                     const float* targets,
+                                     float* gradients,
+                                     int numElements,
+                                     LossType lossType);
 
+void sgd(const float* predictions,
+                 const float* targets,
+                 float* loss,
+                 int numElements,
+                 LossType lossType);
 
-void loss_forward(const float* predictions,
-                  const float* targets,
-                  float* loss,
-                  int numElements,
-                  LossType lossType);
-
+void momentum(const float* predictions,
+                    const float* targets,
+                    float* gradients,
+                    int numElements,
+                    LossType lossType);
 
 #endif 
