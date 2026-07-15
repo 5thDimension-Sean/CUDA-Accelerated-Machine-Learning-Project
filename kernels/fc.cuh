c9598dc817b1a5588452fdcf0e3439ac6529a9c0
Sean Zhang <ss0486034@gmail.com>
2026-07-15 15:05:26 -0400

    added method signatures for backward fc kernels

--- kernels/fc.cuh
+++ kernels/fc.cuh
@@ -5,7 +5,14 @@
 
 __global__ void fc_forward_kernel(const float* X, const float *W, const float *b, float *Y, int batch, int in, int out);
 
+__global__ void fc_backward_weights_kernel(const float *dY, const float*X, float*dW, int batch, int in, int out);
+
+__global__ void fc_backward_bias_kernel(const float *dY, float*db, int batch, int out);
+
+__global__ void fc_backward_input_kernel(const float *dY, const float *W, float *dX, int batch, int in, int out);
 
 void fc_forward(const float* X, const float *W, const float *b, float *Y, int batch, int in, int out);
 
+void fc_backward(const float *dY, const float *X, const float *W,
+                 float *dW, float *db, float *dX, int batch, int in, int out);
 #endif
