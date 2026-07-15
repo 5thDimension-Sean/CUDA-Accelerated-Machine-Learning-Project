384170ad3cf119b590ad27b89cc569a64b17b656
Sean Zhang <ss0486034@gmail.com>
2026-07-15 12:46:06 -0400

    adding header fiels for optimizer.cuh

---
+++ network/optimizer.cuh
@@ -0,0 +1,27 @@
+#ifndef NETWORK_LOSS_CUH
+#define NETWORK_LOSS_CUH
+
+#include <cuda_runtime.h>
+
+enum class LossType
+{
+    MSE,
+    CrossEntropy
+};
+
+__global__ void loss_forward_kernel(const float* predictions,
+                                    const float* targets,
+                                    float* loss,
+                                    int numElements,
+                                    LossType lossType);
+
+
+
+void loss_forward(const float* predictions,
+                  const float* targets,
+                  float* loss,
+                  int numElements,
+                  LossType lossType);
+
+
+#endif 
