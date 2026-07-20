4c70c476ae9e3da97354b993ec76f59039b1fca6
Sean Zhang <ss0486034@gmail.com>
2026-07-20 13:02:40 -0400

    changes to read and created mnist test.py

--- data/read_mnist_test.py
+++ data/read_mnist_test.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 # Read the data back
-data = np.fromfile("mnist_X.bin", dtype=np.float32)
+data = np.fromfile("mnist_test_X.bin", dtype=np.float32)
 data = data.reshape(-1, 784) 
 
 print(data.shape)
 
 
-data1 = np.fromfile("mnist_Y.bin", dtype=np.float32)
+data1 = np.fromfile("mnist_test_Y.bin", dtype=np.float32)
 data1 = data1.reshape(-1, 784) 
 
 print(data1.shape)
