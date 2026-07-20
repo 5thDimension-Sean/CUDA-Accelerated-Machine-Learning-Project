3bfbf4f5440946cd2184e45c226496d621c15df6
Sean Zhang <ss0486034@gmail.com>
2026-07-20 15:36:50 -0400

    update conv2d mc with backwards kernel and wrapper function

--- kernels/conv2d_mc.cuh
+++ kernels/conv2d_mc.cuh
@@ -9,7 +9,17 @@
 // forward kernel (device)
 __global__ void conv2d_mc_forward(const float *input, const float *filter, const float *bias,
                                   float *output, int C_in, int C_out, int H, int W, int FH, int FW);
+            
+__global__ void conv2d_mc_backward(const float *dOut,               
+                        const float *input, const float *filter,
+                        float *dInput, float *dFilter, float *dBias, //3 output
+                        int C_in, int C_out, int H, int W, int FH, int FW);
 
 // host wrapper (malloc + copy + launch + copy-back + free)
 void conv2d_mc(const float *input, const float *filter, const float *bias, float *output,
                int C_in, int C_out, int H, int W, int FH, int FW);
+
+void conv2d_mc_backward(const float *dOut,               
+                        const float *input, const float *filter,
+                        float *dInput, float *dFilter, float *dBias, //3 output
+                        int C_in, int C_out, int H, int W, int FH, int FW);
