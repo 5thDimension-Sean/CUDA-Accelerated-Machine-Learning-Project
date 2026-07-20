206ae3259bd43561d230c2acb29b674cae04e673
Sean Zhang <ss0486034@gmail.com>
2026-07-20 11:13:07 -0400

    prepares mnist by loading data into a file with range 0, 1 inclusive

---
+++ data/prepare_minst.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data = np.loadtxt("mnist_train.csv", delimiter=",", skiprows=1)
+labels = data[:, 0].astype(int)
+pixels = (data[:, 1:] / 255.0).astype(np.float32)  # Normalize pixel values to [0, 1]
+onehot = np.eye(10, dtype=np.float32)[labels]
+
+pixels.tofile("mnist_X.bin")
+onehot.tofile("mnist_Y.bin")
+print("wrote", pixels.shape[0], "samples")
+
