4d201d25223ec6f8bc2270015f583ebb01da7fa1
Sean Zhang <ss0486034@gmail.com>
2026-07-20 11:17:52 -0400

    imports

--- data/mnist_train.cu
+++ data/mnist_train.cu
@@ -1,2 +1,6 @@
-#include "common.cuh"
+#include "common.cuh"       // CUDA_CHECK
+#include "fc.cuh"           // ff/fb
+#include "activations.cuh"  // srs
+#include "loss.cuh"         // ce
+#include "optimizer.cuh"    // sgd
 
