7e629685e8875a17cbc7133e4b7a7aa648b2ca0a
Sean Zhang <ss0486034@gmail.com>
2026-07-20 18:25:05 -0400

    added method signature for conv2d mc

--- kernels/conv2d_mc.cu
+++ kernels/conv2d_mc.cu
@@ -68,7 +68,20 @@ void conv2d_mc(const float *input, const float *filter, const float *bias, float
     CUDA_CHECK(cudaFree(d_out));
 }
 
+__global__ void conv2d_mc_backward_bias(const float *dOut, float *dBias,
+                                        int C_out, int outH, int outW){
 
+}
+
+__global__ void conv2d_mc_backward_weights(const float *dOut, const float *input, float *dFilter,
+                                           int C_in, int C_out, int H, int W, int FH, int FW){
+
+}                             
+
+__global__ void conv2d_mc_backward_input(const float *dOut, const float *filter, float *dInput,
+                                         int C_in, int C_out, int H, int W, int FH, int FW){
+
+}
 
 
 void conv2d_mc_backward(const float *dOut,               
