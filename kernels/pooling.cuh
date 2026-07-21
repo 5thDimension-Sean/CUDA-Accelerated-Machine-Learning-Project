19438014463314de5ae71f0266c2c813368134b9
Sean Zhang <ss0486034@gmail.com>
2026-07-21 15:03:43 -0400

    add the missing functions to the header just in case they will be used

--- kernels/pooling.cuh
+++ kernels/pooling.cuh
@@ -7,6 +7,9 @@
 // --- kernel (device) ---
 __global__ void maxPool2D(const float *input, float *output, int *argmax,
                           int H, int W, int out_H, int out_W, int P, int S);
+__global__ void backMaxPool2D(const float *dOut, const int *argmax, float *dInput, int out_H, int out_W);
 
 // --- host wrapper (malloc + copy + launch + copy-back + free) ---
 void maxPoolWrapKernel(float *h_input, float *h_output, int *argmax, int H, int W, int P, int S);
+
+void backMaxPoolWrapKernel(float *h_dOut, float *h_dInput, int *h_argmax, int H, int W, int P, int S);
