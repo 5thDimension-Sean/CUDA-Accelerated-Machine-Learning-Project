63b7adb62b6cf2b0c386d927e102fc23a353b772
Sean Zhang <ss0486034@gmail.com>
2026-07-25 19:26:07 -0400

    function declaration

--- detection/mnist_detect.cu
+++ detection/mnist_detect.cu
@@ -1,2 +1,17 @@
 #include "common.cuh"
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <chrono>
 
+__global__ void conv2d_mc_forward(const float*, const float*, const float*, float*, int,int,int,int,int,int);
+__global__ void maxPool2D(const float*, float*, int*, int,int,int,int,int,int,int);
+__global__ void backMaxPool2D(const float*, const int*, float*, int,int,int);
+__global__ void conv2d_mc_backward_bias   (const float*, float*, int,int,int);
+__global__ void conv2d_mc_backward_weights (const float*, const float*, float*, int,int,int,int,int,int);
+__global__ void conv2d_mc_backward_input   (const float*, const float*, float*, int,int,int,int,int,int);
+__global__ void fc_forward_kernel         (const float*, const float*, const float*, float*, int,int,int);
+__global__ void fc_backward_weights_kernel(const float*, const float*, float*, int,int,int);
+__global__ void fc_backward_bias_kernel   (const float*, float*, int,int);
+__global__ void fc_backward_input_kernel  (const float*, const float*, float*, int,int,int);
+__global__ void sgd_kernel(float*, const float*, float, int);
