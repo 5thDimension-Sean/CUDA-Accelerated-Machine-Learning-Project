f4a24a3704afa82306de61755e606c1969b1396e
Sean Zhang <ss0486034@gmail.com>
2026-07-26 21:26:14 -0400

    add method signatures for nms.cu, the final FILE!!!!

--- detection/nms.cu
+++ detection/nms.cu
@@ -1,3 +1,24 @@
 #include "common.cuh"
 
 struct Det { float cx, cy, w, h, score; int cls; };
+
+__device__ float iou_dev(float ax, float ay, float aw, float ah,
+                         float bx, float by, float bw, float bh){
+
+
+}
+
+__global__ void nms_kernel(const float* boxes, const float* scores, int n, float iou_thresh, int* keep){
+
+}
+
+int nms(const Det* cand, int n, float iou_thresh, Det* out){
+
+}
+
+#ifndef BUILD_AS_LIBRARY
+int main(){
+
+    return 0;
+}
+#endif
